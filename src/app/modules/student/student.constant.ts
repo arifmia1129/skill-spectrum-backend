c0144bb72466cc726156cea5f0a9aaa989c3fb98
arifmia1129 <arifmia1129@gmail.com>
2023-11-08 07:38:41 +0600

    [DONE] student constant

---
+++ src/app/modules/student/student.constant.ts
@@ -0,0 +1,30 @@
+export const genderEnum = ["male", "female", "other"];
+
+export const bloodGroupEnum = [
+  "a+",
+  "a-",
+  "b+",
+  "b-",
+  "ab+",
+  "ab-",
+  "o+",
+  "o-",
+];
+
+export const StudentSearchableField = [
+  "id",
+  "name.firstName",
+  "name.middleName",
+  "name.lastName",
+  "contactNo",
+  "emergencyContactNo",
+  "email",
+];
+
+export const StudentFilterableField = [
+  "id",
+  "email",
+  "contactNo",
+  "emergencyContactNo",
+  "searchTerm",
+];
