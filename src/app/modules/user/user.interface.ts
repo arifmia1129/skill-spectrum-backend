68598c12ce58c88d28f9685b9fb9662bca5fa815
arifmia1129 <arifmia1129@gmail.com>
2023-11-08 07:49:33 +0600

    [DONE] user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { HydratedDocument, Model, Types } from "mongoose";
+import { IStudent } from "../student/student.interface";
+
+export type IUser = {
+  id: string;
+  role: "student" | "instructor" | "admin";
+  password: string;
+  changePasswordAt: Date;
+  needChangePassword: boolean;
+  student: Types.ObjectId | IStudent;
+  instructor: Types.ObjectId | IStudent;
+  admin: Types.ObjectId | IStudent;
+};
+
+export type IUserMethods = {
+  isUserExist(
+    id: string,
+  ): Promise<Pick<
+    IUser,
+    "id" | "password" | "role" | "needChangePassword"
+  > | null>;
+  isPasswordMatched(
+    givenPassword: string,
+    savedPassword: string,
+  ): Promise<boolean>;
+};
+
+export type UserModel = {
+  createWithFullName(): Promise<HydratedDocument<IUser, IUserMethods>>;
+  // name: string,
+} & Model<IUser, object, IUserMethods>;
