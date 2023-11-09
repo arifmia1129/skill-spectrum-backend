4a19934fad2f65554585692b4db22a04c57b899e
arifmia1129 <arifmia1129@gmail.com>
2023-11-09 12:20:01 +0600

    readme file updated

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,7 +1,7 @@
-import { createLogger, format, transports } from "winston";
-import path from "path";
-import DailyRotateFile from "winston-daily-rotate-file";
-import config from "../config";
+import { createLogger, format /*transports*/ } from "winston";
+// import path from "path";
+// import DailyRotateFile from "winston-daily-rotate-file";
+// import config from "../config";
 
 const { combine } = format;
 
@@ -24,21 +24,21 @@ export const infoLogger = createLogger({
   level: "info",
   format: combine(customTimestamp(), format.json()),
   transports: [
-    config.env === "production"
-      ? new DailyRotateFile({
-          filename: path.join(
-            process.cwd(),
-            "logs",
-            "winston",
-            "success",
-            "%DATE%.log",
-          ),
-          datePattern: "YYYY-MM-DD-HH",
-          zippedArchive: true,
-          maxSize: "20m",
-          maxFiles: "14d",
-        })
-      : new transports.Console(),
+    // config.env === "production"
+    //   ? new DailyRotateFile({
+    //       filename: path.join(
+    //         process.cwd(),
+    //         "logs",
+    //         "winston",
+    //         "success",
+    //         "%DATE%.log",
+    //       ),
+    //       datePattern: "YYYY-MM-DD-HH",
+    //       zippedArchive: true,
+    //       maxSize: "20m",
+    //       maxFiles: "14d",
+    //     })
+    //   : new transports.Console(),
   ],
 });
 
@@ -46,20 +46,20 @@ export const errorLogger = createLogger({
   level: "error",
   format: combine(customTimestamp(), format.json()),
   transports: [
-    config.env === "production"
-      ? new DailyRotateFile({
-          filename: path.join(
-            process.cwd(),
-            "logs",
-            "winston",
-            "error",
-            "%DATE%.log",
-          ),
-          datePattern: "YYYY-MM-DD-HH",
-          zippedArchive: true,
-          maxSize: "20m",
-          maxFiles: "14d",
-        })
-      : new transports.Console(),
+    // config.env === "production"
+    //   ? new DailyRotateFile({
+    //       filename: path.join(
+    //         process.cwd(),
+    //         "logs",
+    //         "winston",
+    //         "error",
+    //         "%DATE%.log",
+    //       ),
+    //       datePattern: "YYYY-MM-DD-HH",
+    //       zippedArchive: true,
+    //       maxSize: "20m",
+    //       maxFiles: "14d",
+    //     })
+    //   : new transports.Console(),
   ],
 });
